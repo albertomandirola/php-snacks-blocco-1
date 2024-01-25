830a0052f4efbad41ce64d830ae35a4d8f02131e
albertomandirola <albymandi00@gmail.com>
2024-01-25 22:44:21 +0100

    complete snack 3

---
+++ snacks-3-SNACK/index.php
@@ -0,0 +1,69 @@
+<?php
+
+$posts = [
+
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+        <?php foreach($posts as $date => $datePost) { ?>
+            <li>
+                <?php echo "<h2>Data: $date</h2>"; ?>
+                <?php foreach ($datePost as $post) { ?>
+                    <?php 
+                        echo "<h3>{$post['title']}</h3>";
+                        echo "<p>Autore: {$post['author']}</p>";
+                        echo "<p>{$post['text']}</p>";
+                        echo "<hr>"; 
+                    ?>
+                <?php } ?>
+            </li>
+         <?php } ?>
+    </ul>
+    
+</body>
+</html>
